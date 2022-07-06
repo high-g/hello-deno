9f385370b497eb6a71a67b966ba62fdd93505ef3
high-g <altema1217bbfumi@live.jp>
2022-07-06 16:49:04 +0900

    modify

--- server.tsx
+++ server.tsx
@@ -1,14 +1,14 @@
-import { serve } from "https://deno.land/std@0.139.0/http/server.ts";
-import { h, ssr } from "https://crux.land/api/get/7GrKMy.ts";
-import "https://deno.land/std@0.37.0";
+/** @jsx h */
+import { serve } from 'https://deno.land/std@0.139.0/http/server.ts'
+import { h, ssr } from 'https://crux.land/api/get/7GrKMy.ts'
 
 const App = () => {
   return (
-    <div class="px-8 py-4">
-      <h1 class="font-semibold text-2xl">Hello Deno</h1>
-      <p class="mt-2 text-gray-500">Welcome to example page!</p>
+    <div className="px-8 py-4">
+      <h1 className="font-semibold text-2xl">Hello Deno</h1>
+      <p className="mt-2 text-gray-500">Welcome to example page!</p>
     </div>
-  );
-};
+  )
+}
 
-serve(() => ssr(() => <App />));
+serve(() => ssr(() => <App />))
